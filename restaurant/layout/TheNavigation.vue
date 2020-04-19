4346d4f25560730d15f20e9460ef78f7a5a8b089
PernilleEmilie <perry-96@live.no>
2020-04-19 12:11:30 +0200

    added toolbar color i navigation

--- restaurant/layout/TheNavigation.vue
+++ restaurant/layout/TheNavigation.vue
@@ -1,7 +1,7 @@
 <template>
     <nav>
-        <v-toolbar>
-            
+        <v-toolbar color="green darken-4">
+            <v-toolbar-title>Italia</v-toolbar-title>
         </v-toolbar>
     </nav>
 </template>
