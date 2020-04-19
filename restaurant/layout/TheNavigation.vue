ae8a662f9e0ccdf90f11ca397e08e6aac51d8333
PernilleEmilie <perry-96@live.no>
2020-04-19 14:35:51 +0200

    navigation change
    Endret på navigation filen

--- restaurant/layout/TheNavigation.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <nav>
-        <v-toolbar color="green darken-4">
-            <v-toolbar-title>Italia</v-toolbar-title>
-        </v-toolbar>
-    </nav>
-</template>
--- restaurant/src/App.vue
+++ restaurant/src/App.vue
@@ -1,5 +1,6 @@
 <template>
 <v-app>
+  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Lora:ital,wght@1,400;1,500&display=swap" rel="stylesheet">
   <div id="app">
     <TheNavigation/>
     <router-view/>
