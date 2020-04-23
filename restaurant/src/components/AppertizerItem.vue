34726af47bde0743af16d616c307dfab97aafbca
PernilleEmilie <perry-96@live.no>
2020-04-23 12:01:47 +0200

    Added listes in appertizer and dish

---
+++ restaurant/src/components/AppertizerItem.vue
@@ -0,0 +1,42 @@
+<template>
+    <article>
+        <v-card>
+            <v-img
+            :src="`https://localhost:5001/images/${imageSrc}`">
+            </v-img>
+            <v-card-title>{{name}}</v-card-title>
+            <v-card-text>
+                {{description}}
+                {{allergies}}
+                {{price}}
+                {{category}}
+            </v-card-text>
+        </v-card>
+    </article>
+</template>
+
+<script>
+export default {
+    props:{
+        name:{
+            type: String
+        },
+        description:{
+            type: String
+        },
+        allergies:{
+            type: String
+        },
+        price:{
+            type: Number
+        },
+        imageSrc:{
+            type: String
+        },
+        category:{
+            type: String
+        }
+    
+    }
+}
+</script>
