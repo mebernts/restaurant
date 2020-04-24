812982007053165b96edaf9e7de006e167367ea8
PernilleEmilie <perry-96@live.no>
2020-04-24 17:28:55 +0200

    Added store
    Store not finish

--- restaurant/src/layout/TheFooter.vue
+++ restaurant/src/layout/TheFooter.vue
@@ -1,5 +1,22 @@
 <template>
     <footer>
-        
+        <h3>Footer</h3>
+        <ul>
+            <li v-for="footer in footerinfo" :key="footer.contactinfo">
+                {{footer.adress}}
+            </li>
+        </ul>
     </footer>
 </template>
+
+<script>
+import FooterStore from '@/Stores/FooterStore.js'
+export default {
+    name: "TheFooter",
+    data(){
+        return{
+            footerinfo: FooterStore.getFooterinfo
+        }
+    }
+}
+</script>
--- restaurant/src/views/menu.vue
+++ restaurant/src/views/menu.vue
@@ -1,5 +1,23 @@
 <template>
     <div>
         <h1>This the menu</h1>
+        <AppertizerItem/>
+        <DishItem/>
+        <DrinkItem/>
     </div>
 </template>
+
+<script>
+import AppertizerItem from '@/components/AppertizerItem.vue'
+import DishItem from '@/components/DishItem.vue'
+import DrinkItem from '@/components/DrinkItem.vue'
+export default {
+    name: 'menu',
+    components: {
+      AppertizerItem,
+      DishItem,
+      DrinkItem
+    } 
+    
+}
+</script>
