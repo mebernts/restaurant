a61eff0112c89aae4d8c8f38cea0dd64bbbddf6a
sigrid <sigrid_linn@hotmail.com>
2020-04-28 15:02:21 +0200

    forsidebilde

--- restaurant/src/views/Home.vue
+++ restaurant/src/views/Home.vue
@@ -2,7 +2,7 @@
 <div>
   <v-img class="imgFluid" src="@/assets/images/header.jpg"
   width="100%"
-  height="300px">
+  height="600px">
    <h1 class="white--text  text-center">Velkommen</h1>
   </v-img>
 </div>
