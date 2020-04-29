2e04c3b7f23956912d175f01745d6593670da61f
Maren Elise Berntsen <maregone@gmail.com>
2020-04-29 15:28:40 +0200

    Update Styling and DB

--- restaurant/src/views/Home.vue
+++ restaurant/src/views/Home.vue
@@ -1,44 +1,53 @@
 <template>
-<div>
-  <v-row>
-    <v-col>
-  <v-img class="imgFluid" src="@/assets/images/header.jpg"
-  width="100%"
-  height="600px">
-   <h1 class="white--text  text-center">Velkommen</h1>
-  </v-img>
-  </v-col>
+  <div>
+    <v-row>
+      <v-col>
+        <v-img class="imgFluid" src="@/assets/images/header.jpg" width="100%" height="600px">
+          <v-container fill-height>
+            <v-layout row wrap align-center>
+              <v-flex class="text-center">
+                <span class="display-3 font-weight-medium white--text">Velkommen</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-img>
+      </v-col>
 
-  <v-col cols="12" sm="6" lg="4">
+      <v-col cols="12" sm="6" lg="4">
+        <v-img class="imgFluid" src="@/assets/images/meny.jpg" width="500px" height="350px">
+        <v-container fill-height>
+            <v-layout row wrap align-center>
+              <v-flex class="text-center">
+                <v-btn class="white--text blue-grey darken-2 text-center font-weight-bold elevation-10" x-large to="/menu" >Se meny her</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          
+        </v-img>
+      </v-col>
 
-  <v-img class="imgFluid" src="@/assets/images/meny.jpg"
-  width="500px"
-  height="350px">
-   <v-btn class="white--text  text-center" to="/menu">Se meny her</v-btn>
-  </v-img>
-  </v-col>
-
-   <v-col cols="12" sm="6" lg="4">
-  <v-img class="imgFluid" src="@/assets/images/matbilde.jpg"
-  width="500px"
-  height="350px">
-  </v-img>
-  </v-col>
-  <v-col cols="12" sm="6" lg="4">
-   <v-img class="align-items imgFluid" src="@/assets/images/about.jpg"
-  width="500px"
-  height="350px">
-   <v-btn class="white--text  text-center" to="/about">Les om oss</v-btn>
-  </v-img>
-  </v-col>
-
-
-  </v-row>
-  
-
-</div>
-
-
+      <v-col cols="12" sm="6" lg="4">
+        <v-img class="imgFluid" src="@/assets/images/matbilde.jpg" width="500px" height="350px"></v-img>
+      </v-col>
+      <v-col cols="12" sm="6" lg="4">
+        <v-img
+          class="align-items imgFluid"
+          src="@/assets/images/about.jpg"
+          width="500px"
+          height="350px"
+        >
+        <v-container fill-height>
+            <v-layout row wrap align-center>
+              <v-flex class="text-center">
+                <v-btn class="white--text blue-grey darken-2 text-center font-weight-bold elevation-10" x-large to="/about">Les om oss</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          
+        </v-img>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
