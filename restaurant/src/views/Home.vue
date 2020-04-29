a676523d91eb032fbeca80e2c0deb640ed41a455
PernilleEmilie <perry-96@live.no>
2020-04-29 14:53:49 +0200

    update home

--- restaurant/src/views/Home.vue
+++ restaurant/src/views/Home.vue
@@ -14,19 +14,9 @@
   <v-img class="imgFluid" src="@/assets/images/meny.jpg"
   width="500px"
   height="350px">
-   <h1 class="white--text  text-center">Se meny her</h1>
+   <v-btn class="white--text  text-center" to="/menu">Se meny her</v-btn>
   </v-img>
   </v-col>
-  <v-col cols="12" sm="6" lg="4">
-
-
-   <v-img class="align-items imgFluid" src="@/assets/images/about.jpg"
-  width="500px"
-  height="350px">
-   <h1 class="white--text  text-center">Les om oss</h1>
-  </v-img>
-  </v-col>
-
 
    <v-col cols="12" sm="6" lg="4">
   <v-img class="imgFluid" src="@/assets/images/matbilde.jpg"
@@ -34,6 +24,14 @@
   height="350px">
   </v-img>
   </v-col>
+  <v-col cols="12" sm="6" lg="4">
+   <v-img class="align-items imgFluid" src="@/assets/images/about.jpg"
+  width="500px"
+  height="350px">
+   <v-btn class="white--text  text-center" to="/about">Les om oss</v-btn>
+  </v-img>
+  </v-col>
+
 
   </v-row>
   
