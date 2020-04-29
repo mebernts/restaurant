ee8ee98c74adf7450f13a54d30c2690d97c5a9c6
PernilleEmilie <perry-96@live.no>
2020-04-29 13:25:37 +0200

    update home

--- restaurant/src/views/Home.vue
+++ restaurant/src/views/Home.vue
@@ -1,22 +1,33 @@
 <template>
 <div>
+  <v-row>
+    <v-col>
   <v-img class="imgFluid" src="@/assets/images/header.jpg"
   width="100%"
   height="600px">
    <h1 class="white--text  text-center">Velkommen</h1>
   </v-img>
+  </v-col>
+
+  <v-col cols="12" sm="6" lg="4">
 
   <v-img class="imgFluid" src="@/assets/images/meny.jpg"
-  width="50%"
+  width="500px"
   height="350px">
    <h1 class="white--text  text-center">Se meny her</h1>
   </v-img>
+  </v-col>
+  <v-col cols="12" sm="6" lg="4">
 
-   <v-img class="imgFluid" src="@/assets/images/about.jpg"
-  width="50%"
+
+   <v-img class="align-items imgFluid" src="@/assets/images/about.jpg"
+  width="500px"
   height="350px">
    <h1 class="white--text  text-center">Les om oss</h1>
   </v-img>
+  </v-col>
+  </v-row>
+  
 
 </div>
 
