56352b339ddb3d4c06d3e80d7023050e042dd550
PernilleEmilie <perry-96@live.no>
2020-04-29 10:04:51 +0200

    change color on ratings

--- restaurant/src/components/AppertizerItem.vue
+++ restaurant/src/components/AppertizerItem.vue
@@ -12,7 +12,7 @@
             <v-card-text>{{category}}</v-card-text>
              <v-rating
              color="amber"
-             background-color="black"
+             background-color="grey lighten-3"
              half-increments
              size="20">
              </v-rating>
